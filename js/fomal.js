1a7aaf7849ee3039d3bcdd867907576cc5d10911
m3ngX1ng <3119391396@qq.com>
2025-02-10 01:56:21 +0800

    Site updated: 2025-02-10 01:56:19

--- js/fomal.js
+++ js/fomal.js
@@ -2769,5 +2769,5 @@ var newYear = () => {
 document.addEventListener('pjax:complete', newYear);
 document.addEventListener('DOMContentLoaded', newYear);
 document.onkeydown = function (e) {
-  if (123 == e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) return btf.snackbarShow("你真坏，不能打开控制台喔!"), event.keyCode = 0, event.returnValue = !1, !1
+  if (123 == e.key || (e.ctrlKey && e.shiftKey && (74 === e.key || 73 === e.key || 67 === e.key)) || (e.ctrlKey && 85 === e.key)) return btf.snackbarShow("你真坏，不能打开控制台喔!"), e.event.keyCode = 0, e.event.returnValue = !1, !1
 };
